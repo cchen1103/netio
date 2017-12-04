05e35d23e44ef52566dea6acb5255e8828953528
chaofeng.chen <chaofeng.chen@gdit.com>
2017-12-03 19:15:07 -0500

    Add timedbucketacounter class

--- counters/counter.py
+++ counters/counter.py
@@ -1,15 +1,32 @@
 from functools import wraps
 
 
+"""
+lambda function to sort on mac, ip, tcp and udp.
+the sorted is used to count number of packets regardless of source and destination
+address as packet is communicates between 2 end points.
+"""
 # x1=src_mac, x2=dst_mac
 _sorted_mac = lambda x1,x2: (x2,x1) if x1 > x2 else (x1,x2)
 # x1=src_mac, x2=dst_mac, y1=src_ip, y2=dst_ip
-_sorted_ip = lambda x1,x2,y1,y2: (x2,y2,x1,y1) if x1 > x2 else (x1,x2,y1,y2)
+_sorted_ip = lambda x1,x2,y1,y2: (x2,x1,y2,y1) if x1 > x2 else (x1,x2,y1,y2)
 # x1=src_mac, x2=dst_mac, y1=src_ip, y2=dst_ip, z1=src_port, z2=dst_port
-_sorted_tcp_udp = lambda x1,x2,y1,y2,z1,z2: (x2,y2,z2,x1,y1,z1) if x1 > x2 else (x1,x2,y1,y2,z1,z2)
+_sorted_tcp_udp = lambda x1,x2,y1,y2,z1,z2: (x2,x1,y2,y1,z2,z1) if x1 > x2 else (x1,x2,y1,y2,z1,z2)
 
 
 class AttrCounter:
+    """
+    class of counter decorator.
+    it count the number of packets for ethernet frame,
+    ip, tcp or udp.
+
+    input:  decoder functions
+    attributes:
+        .counters   count of packets between 2 end points depends on next_protocol
+        .total      total count of packets uses the protocol
+    methods:
+        .clr_count()    clear all counters, or listed end points counters
+    """
     def __init__(self, func):
         self.func = func
         self.counter = dict()
@@ -32,6 +49,37 @@ class AttrCounter:
     @property
     def total(self):
         return sum(self.counters.values())
+
+
+import time
+
+
+class TimedAttrCounter(AttrCounter):
+    def __init__(self, func):
+        """
+        interval in seconds to form the timed buckets based on 00:00:00
+        default interval value is 300 seconds
+        """
+        self._interval = 300
+        self.bucket = int(time()/self.interval)
+        self.counter = dict()
+        self.func = func
+    def __call__(self, *args, **kwargs):
+        new_bucket = int(time()/self.interval)
+        if self.bucket != new_bucket:
+            # snap the time bucket counts from super class
+            # reset super class counters
+            self.counter[self.bucket] = super.counters
+            super.clr_count()
+            self.bucket = new_bucket
+    @property
+    def interval(self):
+        return self._interval
+    @interval.setter
+    def interval(self, val):
+        self._interval = val
+
+
 
 from ..decoders import decoder
 
