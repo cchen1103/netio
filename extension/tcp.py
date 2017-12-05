17f9121fd7dbd52e3b5ca37f78d2bedb94dd553e
chaofeng.chen <chaofeng.chen@gdit.com>
2017-12-05 13:12:29 -0500

    simplify the tcp session counter

--- extension/tcp.py
+++ extension/tcp.py
@@ -29,37 +29,28 @@ def session(func):
         """
         attrs = func(*args, **kwargs)   # tcp has the last attribute as tcp flag
         conn, tcp_flag = attrs[2:-1], attrs[-1] # take out first 2 items of mac addr
-        st = 'a'    # default status of connection is abnormal
+        st = None    # default status of connection is None
         if tcp_flag & (Tcp.syn | Tcp.ack) == Tcp.syn:  # syn only
             if conn in  conn_track:
-                if conn_track[conn] == 's': # time out on syn
-                    st = 't'
+                st = 't' if conn_track[conn] == 's' else 'a'    # time out on syn for mutiple syn othewise abnormal
             else:   # syn
                 st = 's'
                 conn_track[conn] = 's'
-            return _mask_port(conn) + (st,)
         if tcp_flag & (Tcp.syn | Tcp.ack) == Tcp.syn | Tcp.ack: # syn and ack
             conn = _swap_addr(conn)
-            if conn in conn_track:
-                if conn_track[conn] == 's':
-                    st = 'c'    # tcp connection established
-            return _mask_port(conn) + (st,)
+            st = 'c' if conn in conn_track and conn_track[conn] == 's' else 'a'  # tcp connection established on syn/ack
         if tcp_flag & Tcp.rst:    # rst
             conn = _swap_addr(conn)
-            if conn in conn_track:
+            st = 'r' if conn in conn_track else 'a'    # reject
+            if st == 'r':
                 del conn_track[conn]
-                st = 'r'    # reject
-            return _mask_port(conn) + ('r',)
         if tcp_flag & Tcp.fin:  # fin
+            st = 'f' if conn in conn_track or _swap_addr(conn) in conn_track else None
             if conn in conn_track:
                 del conn_track[conn]
-                st = 'f'
-                return _mask_port(conn) + ('f',)
             else:
-                conn = _swap_addr(conn)
-                if conn in conn_track:
-                    del conn_track[conn]
-                    return _mask_port(conn) + ('f',)
+                del conn_track[_swap_addr(conn)]
+        return _mask_port(conn) + (st,) if st else None
     return inner
 
 
