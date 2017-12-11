08f953152834de1012d2d8d69a4bc25396aa708c
chaofeng.chen <chaofeng.chen@gdit.com>
2017-12-11 09:50:27 -0500

    main()

--- netio.py
+++ netio.py
@@ -39,16 +39,32 @@ def main():
         tnc.interval = 10
         for i in range(1000):
             data, addr = s.recvfrom(65535)  # receive all datas from socket
-            nc.update(*decoder.decode_eth(data)[:-1])
-            nc.update(*decoder.decode_ip(data)[:-1])
-            nc.update(*decoder.decode_tcp(data)[:-1])
-            nc.update(*decoder.decode_udp(data)[:-1])
-            tnc.update(*decoder.decode_eth(data)[:-1])
-            tnc.update(*decoder.decode_ip(data)[:-1])
-            tnc.update(*decoder.decode_tcp(data)[:-1])
-            tnc.update(*decoder.decode_udp(data)[:-1])
-            tc.update(*decoder.decode_tcp(data)[:-1])
-            ttc.update(*decoder.decode_tcp(data)[:-1])
+            try:
+                *eth_out, proto = decoder.decode_eth(data)
+                nc.update(*eth_out)
+                tnc.update(*eth_out)
+            except decoder.DecodeException:
+                pass
+            try:
+                *ip_out, proto = decoder.decode_ip(data)
+                nc.update(*ip_out)
+                tnc.update(*ip_out)
+            except decoder.DecodeException:
+                pass
+            try:
+                *tcp_out, proto = decoder.decode_tcp(data)
+                nc.update(*tcp_out)
+                tnc.update(*tcp_out)
+                tc.update(*tcp_out)
+                ttc.update(*tcp_out)
+            except decoder.DecodeException:
+                pass
+            try:
+                *udp_out, proto = decoder.decode_udp(data)
+                nc.update(*udp_out)
+                tnc.update(*udp_out)
+            except decoder.DecodeException:
+                pass
     #print(counter.ethernet_timed_counter.timed_counters)
     #print(counter.ip_timed_counter.timed_counters)
     #print(counter.tcp_timed_counter.timed_counters)
